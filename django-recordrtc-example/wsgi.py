69381e8003453db4c554452de37b26780319dc39
Gilberto Durgante <gilberto.durgante@gmail.com>
2017-08-07 09:53:57 -0300

    Change the project name

--- django-recordrtc-example/wsgi.py
+++ django-recordrtc-example/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for audioteste project.
+WSGI config for django-recordrtc-example project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "audioteste.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-recordrtc-example.settings")
 
 application = get_wsgi_application()
